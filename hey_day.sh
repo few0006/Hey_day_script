fcb1d03ab5abeb4c599f26402cb83430266bb086
few0006 <few0006@auburn.edu>
2024-10-14 14:49:15 -0500

    Add files via upload

---
+++ hey_day.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+
+#Have the user enter their name
+echo "What is your name?"
+
+#The program will then read the name and be able to incoroporate the users response into the output
+read name
+echo "Hey $name!" 
+
+#I will respond with my name 
+echo "My name is Faith!"
+echo "Happy Hey Day!"
+
